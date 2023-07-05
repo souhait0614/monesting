69d6d154e38588aefd84c7e06e6f249ab323a974
すえ <souhait.0614@gmail.com>
2023-07-05 16:58:21 +0900

    format

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
-import { env } from "$env/dynamic/private";
 import { SvelteGoogleAuthHook } from "svelte-google-auth";
 import type { Handle } from "@sveltejs/kit";
+import { env } from "$env/dynamic/private";
 
 const auth = new SvelteGoogleAuthHook({
   client_id: env.AUTH_GOOGLE_CLIENT_ID,
