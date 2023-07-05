641419687ed6b307d38e08e702cf73f3ce828eb1
すえ <souhait.0614@gmail.com>
2023-07-05 16:40:58 +0900

    とりあえずsumiの使用とGoogleサインインができるように

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<script>
+<script lang="ts">
+  import { initialize, signIn, signOut } from "svelte-google-auth/client";
+  import Button from "@smui/button";
+  import type { PageData } from "./$types";
+  import { invalidateAll } from "$app/navigation";
+
+  export let data: PageData;
+  initialize(data, invalidateAll);
 </script>
 
 <svelte:head>
@@ -8,4 +15,8 @@
 
 <section>
   <p>Monesting</p>
+
+  {data.auth.user?.name}
+  <Button on:click={() => signIn()}>Sign In</Button>
+  <Button on:click={() => signOut()}>Sign Out</Button>
 </section>
