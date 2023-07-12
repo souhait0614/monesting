aa9eaa2b502c2f5600534fdb0103672063e65b7f
すえ <souhait.0614@gmail.com>
2023-07-12 18:57:08 +0900

    Row削除

--- src/routes/BackHomeAppBar.svelte
+++ src/routes/BackHomeAppBar.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
   import IconButton from "@smui/icon-button";
-  import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+  import TopAppBar, { Section, Title } from "@smui/top-app-bar";
 
   export let topAppBar: TopAppBar;
 </script>
 
 <TopAppBar bind:this={topAppBar} variant="fixed" color="secondary">
-  <Row>
-    <Section>
-      <IconButton
-        class="material-icons"
-        on:click={() => {
-          history.back();
-        }}>arrow_back</IconButton
-      >
-      <Title>テスト</Title>
-    </Section>
-  </Row>
+  <Section>
+    <IconButton
+      class="material-icons"
+      on:click={() => {
+        history.back();
+      }}>arrow_back</IconButton
+    >
+    <Title>テスト</Title>
+  </Section>
 </TopAppBar>
