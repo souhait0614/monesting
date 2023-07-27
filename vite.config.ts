de3bc51329a17f2e8090846ede6bba58dfde0a27
すえ <souhait.0614@gmail.com>
2023-07-27 10:20:24 +0900

    多分pwaに対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,33 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import { SvelteKitPWA } from "@vite-pwa/sveltekit";
+import { APP_DESCRIPTION, APP_NAME } from "./src/lib/const";
 
-export default defineConfig({
-  plugins: [sveltekit()],
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    plugins: [
+      sveltekit(),
+      SvelteKitPWA({
+        manifest: {
+          name: APP_NAME,
+          short_name: APP_NAME,
+          description: APP_DESCRIPTION,
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: process.env.VITE_PWA_ICON_192_PATH,
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: process.env.VITE_PWA_ICON_512_PATH,
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
