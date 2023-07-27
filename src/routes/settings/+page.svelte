0cefdf35d2b372b800e86faf641314c1b3da989f
すえ <souhait.0614@gmail.com>
2023-07-28 08:30:38 +0900

    デフォルト通貨を実装

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-  import List, { Graphic, Item, PrimaryText, SecondaryText, Text } from "@smui/list";
+  import List, { Graphic, Group, Item, PrimaryText, SecondaryText, Subheader, Text } from "@smui/list";
   import SmuiTopAppBar, { AutoAdjust } from "@smui/top-app-bar";
 
   import Menu from "@smui/menu";
   import TopAppBar from "../_components/BackHomeAppBar.svelte";
-  import { theme } from "../../lib/store";
+  import { theme, defaultCurrency } from "../../lib/store";
   import { THEMES } from "$lib/const";
+  import { CURRENCY_CODES } from "$lib/currencyCodes";
 
   let topAppBar: SmuiTopAppBar;
 
   let themeMenu: Menu;
+  let defaultCurrencyMenu: Menu;
 
   const themeNames = {
     system: "システムに追従",
@@ -24,30 +26,59 @@
 
 <TopAppBar bind:topAppBar title="設定" />
 <AutoAdjust {topAppBar}>
-  <List twoLine>
-    <Item
-      on:SMUI:action={() => {
-        themeMenu.setOpen(true);
-      }}
-    >
-      <Graphic class="material-icons" aria-hidden="true">brightness_6</Graphic>
-      <Text>
-        <PrimaryText>テーマ</PrimaryText>
-        <SecondaryText>{themeNames[$theme]}</SecondaryText>
-      </Text>
-    </Item>
-  </List>
-  <Menu bind:this={themeMenu} anchorCorner="BOTTOM_LEFT">
-    <List>
-      {#each Object.values(THEMES) as id (id)}
-        <Item
-          on:SMUI:action={() => {
-            $theme = id;
-          }}
-        >
-          <Text>{themeNames[id] ?? id}</Text>
-        </Item>
-      {/each}
+  <Group>
+    <Subheader>一般</Subheader>
+    <List twoLine>
+      <Item
+        on:SMUI:action={() => {
+          themeMenu.setOpen(true);
+        }}
+      >
+        <Graphic class="material-icons" aria-hidden="true">brightness_6</Graphic>
+        <Text>
+          <PrimaryText>テーマ</PrimaryText>
+          <SecondaryText>{themeNames[$theme]}</SecondaryText>
+        </Text>
+      </Item>
+      <Item
+        on:SMUI:action={() => {
+          defaultCurrencyMenu.setOpen(true);
+        }}
+      >
+        <Graphic class="material-icons" aria-hidden="true">attach_money</Graphic>
+        <Text>
+          <PrimaryText>デフォルト通貨</PrimaryText>
+          <SecondaryText>{$defaultCurrency}</SecondaryText>
+        </Text>
+      </Item>
     </List>
-  </Menu>
+  </Group>
 </AutoAdjust>
+
+<Menu bind:this={themeMenu} anchorCorner="BOTTOM_LEFT">
+  <List>
+    {#each Object.values(THEMES) as id (id)}
+      <Item
+        on:SMUI:action={() => {
+          $theme = id;
+        }}
+      >
+        <Text>{themeNames[id] ?? id}</Text>
+      </Item>
+    {/each}
+  </List>
+</Menu>
+
+<Menu bind:this={defaultCurrencyMenu} anchorCorner="BOTTOM_LEFT">
+  <List>
+    {#each CURRENCY_CODES as code (code)}
+      <Item
+        on:SMUI:action={() => {
+          $defaultCurrency = code;
+        }}
+      >
+        <Text>{code}</Text>
+      </Item>
+    {/each}
+  </List>
+</Menu>
