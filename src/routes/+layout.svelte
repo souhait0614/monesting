f6046fc29ba9f765c1b74f74f4e5daf925f6b7cb
すえ <souhait.0614@gmail.com>
2023-07-30 16:03:54 +0900

    テーマ問題を暫定対処

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,13 @@
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import { AppContent, Scrim } from "@smui/drawer";
   import { pwaInfo } from "virtual:pwa-info";
+  import { onMount } from "svelte";
   import Drawer from "./_components/MenuDrawer.svelte";
-  import { theme } from "$lib/store";
   import { browser } from "$app/environment";
   import "normalize.css";
   import "@material/typography/mdc-typography.scss";
   import { env } from "$env/dynamic/public";
+  import { theme } from "$lib/store";
 
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -21,6 +22,18 @@
   });
 
   $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : "";
+
+  onMount(() => {
+    const light = document.getElementById("smui-css-light");
+    const dark = document.getElementById("smui-css-dark");
+    if ($theme === "light") {
+      light?.setAttribute("media", "screen");
+      dark?.setAttribute("media", "print");
+    } else {
+      // light.setAttribute("media", "print");
+      dark?.setAttribute("media", "screen");
+    }
+  });
 </script>
 
 <svelte:head>
@@ -28,11 +41,6 @@
   <link rel="apple-touch-icon" sizes="512x512" href={env.PUBLIC_ICON_PATH} />
   <!-- eslint-disable-next-line svelte/no-at-html-tags -->
   {@html webManifestLink}
-  {#if $theme === "light"}
-    <link rel="stylesheet" href="/smui.css" />
-  {:else}
-    <link rel="stylesheet" href="/smui-dark.css" />
-  {/if}
 </svelte:head>
 
 <QueryClientProvider client={queryClient}>
