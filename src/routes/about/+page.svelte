601f13c77072e42cf7d6bc279d3738fa1d9c62e5
すえ <souhait.0614@gmail.com>
2023-08-03 09:37:03 +0900

    リンクをいくつか追加

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,6 +2,7 @@
   import SmuiTopAppBar, { AutoAdjust } from "@smui/top-app-bar";
   import List, { Item, Graphic, Text, PrimaryText, SecondaryText, Group, Subheader } from "@smui/list";
   import Github from "@inqling/svelte-icons/simple-icons/github.svelte";
+  import Misskey from "@inqling/svelte-icons/simple-icons/misskey.svelte";
   import TopAppBar from "../_components/BackHomeAppBar.svelte";
   import { APP_NAME } from "$lib/const";
   import { version } from "$app/environment";
@@ -32,6 +33,41 @@
         <Text>
           <PrimaryText>Github</PrimaryText>
           <SecondaryText>https://github.com/souhait0614/monesting</SecondaryText>
+        </Text>
+      </Item>
+    </List>
+    <Subheader>開発者のリンク</Subheader>
+    <List twoLine>
+      <Item tag="a" href="https://souhait.me" target="_blank">
+        <Graphic class="material-icons" aria-hidden="true">home</Graphic>
+        <Text>
+          <PrimaryText>ホームページ</PrimaryText>
+          <SecondaryText>https://souhait.me</SecondaryText>
+        </Text>
+      </Item>
+      <Item tag="a" href="https://submarin.online/@souhait" target="_blank">
+        <Graphic aria-hidden="true">
+          <Misskey />
+        </Graphic>
+        <Text>
+          <PrimaryText>Misskey</PrimaryText>
+          <SecondaryText>https://submarin.online/@souhait</SecondaryText>
+        </Text>
+      </Item>
+      <Item tag="a" href="https://github.com/souhait0614" target="_blank">
+        <Graphic aria-hidden="true">
+          <Github />
+        </Graphic>
+        <Text>
+          <PrimaryText>Github</PrimaryText>
+          <SecondaryText>https://github.com/souhait0614</SecondaryText>
+        </Text>
+      </Item>
+      <Item tag="a" href="https://x.com/ElectronicsBot" target="_blank">
+        <Graphic aria-hidden="true"><span style="font-size:30px;">𝕏</span></Graphic>
+        <Text>
+          <PrimaryText>X</PrimaryText>
+          <SecondaryText>https://x.com/ElectronicsBot</SecondaryText>
         </Text>
       </Item>
     </List>
