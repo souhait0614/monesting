766a1cda0129f2b03f45687f79734218162620ef
すえ <souhait.0614@gmail.com>
2024-08-27 16:32:09 +0900

    NextAuth

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
+import NextAuthProvider from './providers/NextAuthProvider';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthProvider>
+          {children}
+        </NextAuthProvider>
+      </body>
     </html>
   );
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { center } from 'styled-system/patterns';
+
 import { Button } from '~/components/ui/button';
 
 export default function Home() {
+  const { data, status } = useSession();
+
+  if (status === 'loading') {
+    return (
+      <div className={center({
+        height: '100vh',
+      })}
+      >Loading...
+      </div>
+    );
+  }
+  if (status !== 'authenticated') {
+    return (
+      <div className={center({
+        height: '100vh',
+      })}
+      >
+        <Button onClick={() => signIn('google')}>さいんいん</Button>
+      </div>
+    );
+  }
   return (
-    <Button>Test</Button>
+    <div className={center({
+      height: '100vh',
+    })}
+    >
+      <p>{data.user?.email}</p>
+      <Button onClick={() => signOut()}>さいんあうと</Button>
+    </div>
   );
+
 }
