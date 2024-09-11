49efd40167de06b72ce44b00fcf0673b4f75b626
すえ <souhait.0614@gmail.com>
2024-09-11 22:15:51 +0900

    format

--- apps/web/src/app/providers/NextAuthProvider.tsx
+++ apps/web/src/app/providers/NextAuthProvider.tsx
@@ -3,8 +3,8 @@
 import { SessionProvider } from 'next-auth/react';
 import type { ReactNode } from 'react';
 
-const NextAuthProvider = ({ children }: { children: ReactNode; }) => {
+function NextAuthProvider({ children }: { children: ReactNode; }) {
   return <SessionProvider>{children}</SessionProvider>;
-};
+}
 
 export default NextAuthProvider;
