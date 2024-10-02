3d4cfe8c732f948487a1d8260087415aa68ac9b9
すえ <souhait.0614@gmail.com>
2024-10-02 15:40:01 +0900

    フォルダを移動

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-import NextAuthProvider from './providers/NextAuthProvider';
+import NextAuthProvider from './_providers/NextAuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
